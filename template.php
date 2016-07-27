a32b777bf980d4381ecc7c5efb56a954550e14ec
Assasin <assasin@sinair.ru>
2016-07-27 03:58:11 +0300

    пайп-функция генератора пагинатора

--- template.php
+++ template.php
@@ -10,6 +10,34 @@ Template::addUserFunctionHandler(function ($v, $func, $fargs){
 		case 'usercolor':
 			$usr = new User(+$v);
 			return $usr->getColoredName();
+		
+		case 'humanbytes':
+			return translateBytes(+$v);
+			
+		case 'build_pagination':
+			$cur = $fargs[0];
+			$count = $v;
+			$CNT = $fargs[1];
+			
+			$res = [1];
+			$i = 1;
+			if ($i < $count){
+				if ($i < $cur - $CNT)
+					$res[] = '...';
+		
+				for ($i = max($i, $cur - $CNT); $i < $count - 1 && $i < $cur + $CNT + 1; ++$i){
+					$res[] = $i+1;
+				}
+	
+				if ($i < $count - 1){
+					$res[] = '...';
+				}
+				
+				$res[] = $count;
+			}
+
+			return $res;
+			
 	}
 	
 	return $v;
