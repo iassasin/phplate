604f132ba575906b7c22307228b7ec5120985da2
iassasin <iassasin@yandex.ru>
2017-07-05 16:07:16 +0300

    Исправлен нерабочий пример пути (требуется / в конце)

--- template.php
+++ template.php
@@ -2,7 +2,7 @@
 
 require_once 'template_base.php';
 
-Template::init($_SERVER['DOCUMENT_ROOT'].'/resources/templates');
+Template::init($_SERVER['DOCUMENT_ROOT'].'/resources/templates/');
 Template::addUserFunctionHandler(function ($v, $func, $fargs){
 	$facnt = count($fargs);
 	
