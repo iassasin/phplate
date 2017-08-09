0b2e96bfb6e66a0aba861737bdc0295f07f7f33c
iassasin <iassasin@yandex.ru>
2017-08-10 01:40:17 +0300

    Добавлен тест устанавливаемости опций

---
+++ tests/TemplateOptionsTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Author: maestroprog <maestroprog@gmail.com>
+ * License: beerware
+ * Use for good
+ */
+
+use Iassasin\Phplate\TemplateOptions;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Iassasin\Phplate\TemplateOptions
+ */
+class TemplatOptionsTest extends TestCase {
+	public function testTemplateOptions(){
+		$opt = (new TemplateOptions())
+			->setCacheEnabled(true)
+			->setAutoSafeEnabled(true)
+			->setDateFormat('[Y-m-d H:i:s]')
+		;
+
+		$this->assertEquals(true, $opt->getCacheEnabled());
+		$this->assertEquals(true, $opt->getAutoSafeEnabled());
+		$this->assertEquals('[Y-m-d H:i:s]', $opt->getDateFormat());
+	}
+}
